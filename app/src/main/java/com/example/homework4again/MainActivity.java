2c19781c1f52ff292561c313f3e6cfff74e281c1
user9-10-21 <youmansuck2@gmail.com>
2021-09-30 22:31:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/homework4again/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.homework4again;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
